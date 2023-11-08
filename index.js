e5c99030b22e2c736361cf4a4daf2c0c232905ac
NourIssa7 <nouracm7@gmail.com>
2023-11-08 12:09:30 -0500

    path and core modules

---
+++ index.js
@@ -0,0 +1,5 @@
+ const Person = require('./person'); //file not module so .
+//import Person from "./person";
+const person1= new Person('ali',30);
+person1.greeting();
+console.log('h',' rferfre');
